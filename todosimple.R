aa2739c9291737e4992f56bef3fca7d21d0184f4
Hieu Le <lehieu.htl@outlook.com>
2023-11-15 18:03:35 -0600

    basics

--- todosimple.R
+++ todosimple.R
@@ -1,2 +1,45 @@
 install.packages('shiny')
 install.packages('shinydashboard')
+library(shiny)
+library(shinydashboard)
+
+#Creating a simple to-doList
+ui = dashboardPage(
+  dashboardHeader(title = "To-do List Simple"),
+  dashboardSidebar(),
+  dashboardBody(
+    #Add task Input
+    textInput("task", "Task:"),
+    actionButton("add", "Add Task"),
+    
+    #Display todo list Output
+    h3("What am i doing?"),
+    verbatimTextOutput("todolist")
+  )
+)
+
+#Server logic
+server = function(input, output) {
+  tasks = reactiveVal(character(0))
+  observeEvent(input$add, {
+    new_task = isolate(input$task)
+    if (new_task != "") {
+      tasks(c(tasks(), new_task))
+      updateTextInput(session, "task", value = "")
+    }
+  })
+  #Display the list
+  output$todolist = renderPrint({
+    tasks()
+  })
+}
+
+shinyApp(ui, server)
+runApp(Users/hieule/Desktop/MiscProjects/R.projects/todo)
+
+
+
+
+
+
+
